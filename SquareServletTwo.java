bbfb368c38bc1b6331e437a9176482762a09725a
Kabin_giri <85396529+Kabin119245@users.noreply.github.com>
2024-04-25 20:26:44 +0545

    Create SquareServletTwo.java

---
+++ SquareServletTwo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.kabin.mavenproject1;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author girik
+ */
+public class SquareServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+
+//      int k = (int)req.getAttribute("k");
+        int k = Integer.parseInt(req.getParameter("k"));
+        out.println("Sum  is " + k);
+        k = k * k;
+
+        out.println("Sqaured after sum is " + k);
+
+        System.out.println("Square servlet");
+
+    }
+}
